cbb3f406f39591e13a29a6e4cfc8f34b682a05e9
Toyosi <toyosibamidele@gmail.com>
2023-03-06 19:26:08 -0500

    updated path

--- nb/front_end.py
+++ nb/front_end.py
@@ -21,7 +21,7 @@ plt.style.use('seaborn-pastel')
 
 
 def load_data():
-    return pd.read_csv('https://github.com/toyobam92/Group-By-Project---FourthBrain-/tree/uplift_steps/dat/feature_eng_data.csv')
+    return pd.read_csv('https://raw.githubusercontent.com/toyobam92/Group-By-Project---FourthBrain-/uplift_steps/dat/feature_eng_data.csv')
 
 
 def create_plot(df, col, plot_type):
